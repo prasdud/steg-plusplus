20d3d7e2195defa078f97c504f43a4acf4cc0df3
prasdud <prasdudwastaken@gmail.com>
2024-09-18 22:07:54 +0530

    coded pixel extractor

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <fstream>
+
 
 int main() {
     std::cout << "OpenCV version: " << CV_VERSION << std::endl;
@@ -11,6 +13,39 @@ int main() {
     {
     	std::cout << "error :: image cant be loaded"<<std::endl;
     	return -1;
+    }
+
+    std::ofstream outfile("image_dat.rgb");
+    if (!outfile) {
+        std::cerr << "Error opening output file." << std::endl;
+        return -1;
+    }
+
+    for( int i = 0 ; i < image.rows ; i++)
+    {
+        for( int j = 0 ; j < image.cols ; j++ )
+        {
+            if(image.type() == CV_8UC1)
+            {
+                image.at<uchar>(i,j) = 255;
+            }
+            else if(image.type() == CV_8UC3)
+            {
+                std::cout << (int)image.at<cv::Vec3b>(i,j)[0] << " " << (int)image.at<cv::Vec3b>(i,j)[1] << " " << (int)image.at<cv::Vec3b>(i,j)[2] << std::endl;
+                outfile << (int)image.at<cv::Vec3b>(i,j)[0] << " " << (int)image.at<cv::Vec3b>(i,j)[1] << " " << (int)image.at<cv::Vec3b>(i,j)[2] << std::endl;
+
+                //image.at<cv::Vec3b>(i,j)[0] = 255;
+                //image.at<cv::Vec3b>(i,j)[1] = 255;
+                //image.at<cv::Vec3b>(i,j)[2] = 255;
+
+                //std::cout << (int)image.at<cv::Vec3b>(i,j)[0] << " " << (int)image.at<cv::Vec3b>(i,j)[1] << " " << (int)image.at<cv::Vec3b>(i,j)[2] << std::endl;
+            }
+            else
+            {
+                std::cout << "Unknown image format" << std::endl;
+                return 0;
+            }
+        }
     }
 
     cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
